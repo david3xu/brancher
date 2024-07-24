678af29c079c19dcc8fc7270319450ad5ebfac8f
David <291928k@curtin.edu.au>
2024-07-24 15:21:50 +0800

    add no cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+// export const dynamic = "force-dynamic"; // force dynamic, fix caching 
 
-export const dynamic = "force-dynamic"; // force dynamic, fix caching 
 
 export default async function Home() {
+  const cookieJar = cookies();
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -11,7 +13,7 @@ export default async function Home() {
   const { data } = await supabase.from("posts").select("*, comments(*)");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify({data, cookieJar}, null, 2)}</pre>
     </main>
   );
 }
